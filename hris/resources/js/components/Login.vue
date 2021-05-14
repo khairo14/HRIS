421810beec384fe01bb17afa80de6931e79f1935
khairo14 <eldrin.bradecina@gmail.com>
2021-05-15 05:56:44 +0800

    create login component/edit welcome blade

---
+++ hris/resources/js/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <div class="wrapper">
+        <div class="container">
+            <h5>Login Form</h5>
+            <div class="form-group">
+                <form action="">
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+</template>
+<script>
+export default {
+
+}
+</script>
