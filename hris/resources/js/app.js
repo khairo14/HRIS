bc3b6821b5291335d8b031902b21950f5932204b
khairo14 <eldrin.bradecina@gmail.com>
2021-05-30 15:51:41 +0800

    vuetify added-removing main css

--- hris/resources/js/app.js
+++ hris/resources/js/app.js
@@ -3,10 +3,12 @@ require('./bootstrap');
 window.Vue = require('vue').default;
 
 import router from './router'
+import Vuetify from '../plugins/vuetify'
 //Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('main-component', require('./components/main.vue').default);
 
 const app = new Vue({
     el: '#app',
-    router
+    router,
+    vuetify: Vuetify
 });
--- hris/resources/js/components/main.vue
+++ hris/resources/js/components/main.vue
@@ -1,29 +1,12 @@
 <template>
-<div class="wrapper">
-    <!-----header------------->
-        <div class="header">
-            <div class="col-md">
-                <h5>profile</h5>
-            </div>
-        </div>
-    <!-----header------------->
-    <!-----sidebar------------->
-        <div class="navi">
-            <h5>Logo</h5>
-            <ul>
-                <li>
-                    <router-link to="/">Home</router-link>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <router-link to="/login">Login</router-link>
-                </li>
-            </ul>
-        </div>
-    <!-----sidebar------------->
-    <router-view/>
-</div>
+    <v-app>
+        <v-main>
+            <v-btn>
+            test
+            </v-btn>
+        </v-main>
+        <router-view></router-view>
+    </v-app>
 </template>
 
 <script>
